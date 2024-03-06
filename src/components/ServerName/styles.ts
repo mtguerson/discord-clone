c405c5e5e8b96316d9158ca4d7b77bcdd31976be
Matheus Guerson <mtguerson@gmail.com>
2024-03-06 14:54:04 -0400

    creating and starting the ServerName development

---
+++ src/components/ServerName/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 11px 0 16px;
+
+
+  background-color: var(--secondary);
+`;
+
+export const Title = styled.div``;
+
+export  const ExpandIcon = styled.div``;
